4ef6d7705d9b7d7f08eea967112f216e3d89296a
shrouk-mokhles <shroukm146@gmail.com>
2026-01-26 15:50:51 +0200

    update .env

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from "dotenv";
-import path from "path";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
 
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+dotenv.config();
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
