b5160f8964395a53eea1cc33caca6266ac5b90e6
shrouk-mokhles <shroukm146@gmail.com>
2026-01-26 14:38:32 +0200

    update .env

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from "dotenv";
+import path from "path";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "https://be.islam-xplore.betazssone.xyz/api",
+    baseURL: process.env.API_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
